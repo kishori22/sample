ea7a32fd557a6a2d8906336bdf82bbf9aff3ed62
heman <heman@10.0.0.208>
2022-06-09 12:32:26 -0500

    first commit

---
+++ src/Demo/java/Demo.java
@@ -0,0 +1,13 @@
+package Demo.java;
+
+  public class Demo {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello,");
+		System.out.println("Welcome to Java");
+	
+	}
+ }
+
+
---
+++ src/Demo/java/result.java
@@ -0,0 +1,14 @@
+package Demo.java;
+
+public class result { 
+	public static void main(String[] args) {
+          
+		int A = 5;
+		int B = 10;
+		int sum = A+B;
+	System.out.println(sum);
+	  if (sum > 10) {
+    System.out.println("This is correct");  		
+}
+}
+}
